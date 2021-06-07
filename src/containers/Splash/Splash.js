8f8b863993d02575583d05b1b09ab8f8eb9862c5
Amr El Mahdy <amr.elmahdy@vodafone.com>
2021-06-07 06:12:06 +0200

    fix: enhance splash screen

--- src/containers/Splash/Splash.js
+++ src/containers/Splash/Splash.js
@@ -1,25 +1,13 @@
 import React, {useEffect} from 'react';
-import {
-  View,
-  Text,
-  Image,
-  ImageBackground,
-  Dimensions,
-  Platform,
-} from 'react-native';
-import styles from './Splash.Styles';
-import {Images} from './../../theme/images';
+import {View} from 'react-native';
 import {NavigationServices} from './../../api/NavigationService';
 import {getItemFromAsyncStorage} from './../../api/helpers';
 import {setCurrentUser} from './../../store/actions/userActions';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import RNBootSplash from 'react-native-bootsplash';
 
-const SplashScreen = ({navigation}) => {
+const SplashScreen = () => {
   const dispatch = useDispatch();
-
-  const user = useSelector(state => state.auth.User);
-
   useEffect(() => {
     getItemFromAsyncStorage('@auth-user')
       .then(res => {
@@ -27,7 +15,7 @@ const SplashScreen = ({navigation}) => {
           const user = JSON.parse(res);
           dispatch(setCurrentUser(user));
         }
-        navigate(res)
+        navigate(res);
       })
       .catch(err => {
         console.log(err);
